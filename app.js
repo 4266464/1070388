d1290366f958eee4689a5596372d0136c39549f4
4266464 <35630677+4266464@users.noreply.github.com>
2024-01-19 00:28:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { getlist, getVerify, create } = require('./api.js')
 const TOKEN = process.env.TOKEN || null
 const UID = process.env.UID || null
 
-let message = '蒸蒸日上'
+let message = '蒸蒸日上'+process.env.UID
 
 
 if (!TOKEN || !UID) return
